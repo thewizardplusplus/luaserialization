de4e899d1fe633bf304b9825bf00e3cd38006ac9
thewizardplusplus <thewizardplusplus@yandex.ru>
2024-06-03 17:20:31 +0500

    Add the interface test for the `FileForStdLibrary` class

---
+++ filesystem/fileforstdlibrary_test.lua
@@ -0,0 +1,16 @@
+local luaunit = require("luaunit")
+local FileForStdLibrary = require("luaserialization.filesystem.fileforstdlibrary")
+local filesystem = require("luaserialization.filesystem.filesystem")
+
+-- luacheck: globals TestFileForStdLibrary
+TestFileForStdLibrary = {}
+
+function TestFileForStdLibrary.test_interface()
+  local dummy_inner_file = { id = 42 }
+  local file = FileForStdLibrary:new(dummy_inner_file)
+
+  local result = filesystem.is_file(file)
+  luaunit.assert_true(result)
+
+  luaunit.assert_equals(file._inner_file, dummy_inner_file)
+end
